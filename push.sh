6e9b4cc327a15e14c62e79ee823310a0ebf2e568
zuogerenba <1732935973@qq.com>
2021-02-17 22:18:34 +0800

    update blog

---
+++ push.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+git add -A
+git commit -m 'update blog'
+
+# 如果发布到 https://<USERNAME>.github.io
+git push
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
